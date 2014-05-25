04925b746f2dfd4344abca3e6ae1a7fad253c1d9
irulenot <anthony.i.bilic@gmail.com>
2014-05-25 01:32:35 -0700

    "changed style to css"

--- jquery.js
+++ jquery.js
@@ -1,13 +1,7 @@
 // jquery
 
-// var x = 2;
+var block_colors = {"","","","","","","","",""}
 
-// var y = "Apple";
+function colorChange(){
 
-// var z = 1;
-
- 
-
-// alert(x+z); //Will give us 1!
-
-// alert(x+y+z);//Will give us "2Apple1", whatever that means.
+}
