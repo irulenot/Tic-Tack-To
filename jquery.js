9f11053a80a9c4eccbdfd81c7e1076c17aa3c634
irulenot <anthony.i.bilic@gmail.com>
2014-05-25 02:13:27 -0700

    "Derrick's changes"

--- jquery.js
+++ jquery.js
@@ -1,7 +1,36 @@
 // jquery
 
-var block_colors = {"","","","","","","","",""}
 
-function colorChange(){
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var block_colors = {"","","","","","","","",""}
+
+// function colorChange(){
+
+// }
+// $(document).ready(function(){
+// 	var colors = ['red', 'blue']
+// 	  , current = 0
+// 	setInterval(function(){
+// 		console.log(current)
+// 		$('#word1').css({
+// 			'color': colors[current]
+// 		})
+// 		current = current ? 0 : 1
+// 	}, 500)
+
+	
+})
