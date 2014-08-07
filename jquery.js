8a6848992a9f23ec7785b9cd4b277d5da79cc2fb
Bruce Lin <brucelin1218@gmail.com>
2014-08-06 22:48:11 -0700

    argin fix

--- jquery.js
+++ jquery.js
@@ -2,12 +2,16 @@
 $(document).ready(function(){ 
   //when mouse hovers, class "block" opacity changes
   //"this" refers to .block
+
   $('.block').mouseenter(function(){
   	$(this).fadeTo('fast', 1);
+  	
   });
   $('.block').mouseleave(function(){
     $(this).fadeTo('fast', 0.6);
+   
   });
+
 });
 
 
