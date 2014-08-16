53240f6a96d361dd9ef6c1a7de3013cc50266693
Anthony Bilic <anthonybilic@anthonys-mbp.sx76x.gigaset.net>
2014-08-16 13:37:56 +0200

    Cleaned up jquery.js

--- jquery.js
+++ jquery.js
@@ -1,48 +1,67 @@
-//the std start
 $(document).ready(function(){ 
 
-  //when mouse hovers, class "block" opacity changes
-  //"this" refers to .block
+  /**
+  @title     Cursor enter fading
+  */
   $('.block').mouseenter(function(){
   	$(this).fadeTo('fast', 1);
   });
 
+  /**
+  @title    Cursor leave fading
+  */
   $('.block').mouseleave(function(){
     $(this).fadeTo('fast', 0.6);
    });
 
+  /**
+  @title     Variables
+  */
   var colorName = ["Blue","Red","Orange","Green","Yellow","Peach","Lime","Gray","Purple"];
   var colorCode = ["#59ABE3","#F22613","#FF6600","#00B16A","#F7CA18","#f86767","#7ec9b1","#6c6c6c","#c091e6"]
   var block     = ["block1","block2","block3","block4","block5","block6","block7","block8","block9"];
 
-  // changes color of block
+  /**
+  @title    Change block color
+  @param    [block] = target block, [colorCode] = color code changed to
+  @returns  Modifies html block colors
+  */
   function colorChange (block, colorCode){
     document.getElementById(block).style.backgroundColor = colorCode;
-  } //example of how to call: colorChange(block[0], colorCode[3]);
+  }
 
+  /**
+  @title    Get random number
+  @param    [num] = determines range of random numbers
+  @returns  Random number
+  */
   function getRandom(num) {
     var num = "0" + Math.random() * num;
     return num[1];
   }
 
-    function colorShuffle(){
+  /**
+  @title    Random block color shuffle
+  @param    Uses (colorName) and (block)
+  @returns  Changes colors of blocks at random with no overlap
+  */
+  function colorShuffle(){
     for(i = 0; i < 9; i++){
-      chosen = getRandom(8-i); //(random #) 5
-      container = colorCode[chosen]; // holds random color code
+      chosen = getRandom(8-i);
+      container = colorCode[chosen];
 
-      colorChange(block[i],container); // makes block i [0] turn into random color [5]
+      colorChange(block[i],container);
 
       colorCode[chosen] = colorCode[8-i];
       colorCode[8-i] = container;
     }
   }
 
-  //SPECS:
-
-  //generate random word
-  //randomize colors and words
-  //click verification of word
-  //verification ends with end game or increment of time
+  //To do:
+    //generate random word
+    //randomize words
+    //click verification of word
+    //verification ends with end game or increment of time
 
   colorShuffle();
 
